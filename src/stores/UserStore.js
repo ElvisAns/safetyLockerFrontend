4759858c6612be3df798d04a2109b304efba37f7
ElvisAns <ansimapersic@gmail.com>
2022-11-12 22:07:50 +0100

    enh : initial state for the user store

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -3,7 +3,9 @@ import { defineStore } from "pinia";
 export const useUserStore = defineStore('user_infos',{
     state : ()=>{
         return {
-            
+            hasPassed_onboarding : localStorage.getItem('onboarding')||false,
+            username : localStorage.getItem("username"),
+            telephone : localStorage.getItem("telephone"),
         }
     }
 })
