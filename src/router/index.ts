4321261b1e1bc8cc4c478a0883b93b6159dd8425
ElvisAns <ansimapersic@gmail.com>
2022-11-12 11:43:27 +0100

    fix : use semantics urls in the router

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import TabsPage from '../views/TabsPage.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/tabs/dashboard'
   },
   {
     path: '/tabs/',
@@ -13,18 +13,18 @@ const routes: Array<RouteRecordRaw> = [
     children: [
       {
         path: '',
-        redirect: '/tabs/tab1'
+        redirect: '/tabs/dashboard'
       },
       {
-        path: 'tab1',
+        path: 'actions',
         component: () => import('@/views/Tab1Page.vue')
       },
       {
-        path: 'tab2',
+        path: 'dashboard',
         component: () => import('@/views/Tab2Page.vue')
       },
       {
-        path: 'tab3',
+        path: 'settings',
         component: () => import('@/views/Tab3Page.vue')
       }
     ]
