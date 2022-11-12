d2e796c2612bff78d4d16cddfaed793a8ea1c75d
ElvisAns <ansimapersic@gmail.com>
2022-11-12 11:43:52 +0100

    feat : added pinia to manage our app states

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import { createPinia } from 'pinia';
 
 import { IonicVue } from '@ionic/vue';
 
@@ -23,8 +24,11 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+const pinia  = createPinia()
+
 const app = createApp(App)
   .use(IonicVue)
+  .use(pinia)
   .use(router);
   
 router.isReady().then(() => {
