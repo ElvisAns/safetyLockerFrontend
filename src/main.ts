b9c8e9515e01755b5c49ce6e816ad4b6de664973
ElvisAns <ansimapersic@gmail.com>
2022-11-12 22:04:48 +0100

    enh : defaulty set the mode to ios for better UI

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import router from './router';
 import { createPinia } from 'pinia';
 
 import { IonicVue } from '@ionic/vue';
-
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
 
@@ -23,14 +22,13 @@ import '@ionic/vue/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-
 const pinia  = createPinia()
 
 const app = createApp(App)
-  .use(IonicVue)
+  .use(IonicVue,{mode:'ios'})
   .use(pinia)
   .use(router);
-  
+
 router.isReady().then(() => {
   app.mount('#app');
 });
