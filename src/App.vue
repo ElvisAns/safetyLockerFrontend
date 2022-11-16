dcb1d5cd480125955a0a92a139640255aadbf559
ElvisAns <ansimapersic@gmail.com>
2022-11-16 14:16:38 +0100

    enh : add minimalist splash screen mock

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <template>
   <ion-app>
+    <div v-show="displaySplash" :class="animateClass" class="animate__animated splash"><img :src="require('/public/assets/icon/icon.png')"></div>
     <ion-router-outlet />
   </ion-app>
 </template>
 
-<script lang="ts">
+<script lang="js">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import 'animate.css';
 
 export default defineComponent({
   name: 'App',
   components: {
     IonApp,
     IonRouterOutlet
+  },
+  data(){
+    return{
+      displaySplash:true,
+      animateClass:""
+    }
+  },
+  mounted(){
+    setTimeout(()=>{
+      this.animateClass = "animate__fadeOut"
+    },2000);
+    setTimeout(()=>{
+      this.displaySplash=false
+    },3000);
   }
 });
 </script>
+
+<style scoped>
+  .splash{
+    background: #3dc2ff;
+    width:100vw;
+    height: 100vh;
+    position:absolute;
+    top:0;
+    left:0;
+    z-index: 900000;
+    display: grid;
+    place-items: center;
+  }
+</style>
