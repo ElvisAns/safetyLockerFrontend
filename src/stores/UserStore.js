8088997637b555aeb51a4f357f71785a57db1137
ElvisAns <ansimapersic@gmail.com>
2022-11-16 09:15:05 +0100

    enh : add getters and actions to the userstore

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -1,11 +1,26 @@
 import { defineStore } from "pinia";
 
 export const useUserStore = defineStore('user_infos',{
-    state : ()=>{
-        return {
+    state : ()=>({
             hasPassed_onboarding : localStorage.getItem('onboarding')||false,
             username : localStorage.getItem("username"),
             telephone : localStorage.getItem("telephone"),
+            lockingPin : localStorage.getItem("lockingPin")
+    }),
+    getters:{
+        current_user_name:(state)=>state.username,
+        current_phone: (state)=>state.telephone,
+        user_has_onboard: (state)=>state.hasPassed_onboarding,
+        get_current_saved_pin : (state)=>state.lockingPin
+    },
+    actions:{
+        save_user_data(payload){
+            this.username = payload.username !== null?payload.username:"";
+            this.telephone =  payload.telephone !== null?payload.telephone:"";
+            this.lockingPin = payload.lockingPin !== null?payload.lockingPin:"";
+            localStorage.setItem("username",this.username)
+            localStorage.setItem("telephone",this.telephone)
+            localStorage.setItem("lockingPin",this.lockingPin)
         }
     }
 })
